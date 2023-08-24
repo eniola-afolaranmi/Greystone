a582ebd7b39eefab8d23f0b380de65933058337e
Eniola156 <92401187+Eniola156@users.noreply.github.com>
2023-08-24 09:40:31 -0700

    Removed wImpact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -45,20 +45,6 @@ export default function Home() {
         </Link>
 
         <Link
-          href="wImpact"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Whats Impact?{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-        </Link>
-
-        <Link
           href="/senseiLogin"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
