0a6304d232f61ba338f153dd6620fec3a887ee36
Eniola156 <92401187+Eniola156@users.noreply.github.com>
2023-08-24 08:51:10 -0700

    wsdfghj
    uij

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export {default as Feature} from './feature/Feature';
 export {default as NavBar} from './navbar/Navbar';
+export { default as Level } from "./level/Level";
 export {default as SenseiLevel} from "./SenseiLevel/SenseiLevel";
